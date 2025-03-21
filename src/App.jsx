2194edc54b147ed7f6c49b3f9954e6aa93fa9317
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-03-21 20:59:17 +1100

    revise react concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Card({ title }) {
+	const [count, setCount] = useState(0);
+	const [hasLiked, setHasLiked] = useState(false);
+	useEffect(() => {
+		console.log(`${title} has been liked: ${hasLiked}`);
+	}, [hasLiked]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// const [varibleName, setVariableName] = useState()
+	// [initial value, updater function]
+	// const [hasLiked, setHasLiked] = useState(false);
+
+	return (
+		<div
+			className="card"
+			onClick={() => setCount((prevState) => prevState + 1)}
+		>
+			<h2>
+				{title} <br />
+				{count}
+			</h2>
+			<button onClick={() => setHasLiked((prevState) => !prevState)}>
+				{hasLiked ? '🤎' : '🤍'}
+			</button>
+		</div>
+	);
 }
 
-export default App
+function App() {
+	return (
+		<div className="card-container">
+			<Card title="Star Wars" />
+			<Card title="Avatar" />
+			<Card title="Dune" />
+		</div>
+	);
+}
+
+export default App;
