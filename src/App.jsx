b8c32a03f40b0839483057f91207ef1e0d402a85
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-03-21 22:04:36 +1100

    add images and search term functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useEffect, useState } from 'react';
-
-function Card({ title }) {
-	const [count, setCount] = useState(0);
-	const [hasLiked, setHasLiked] = useState(false);
-	useEffect(() => {
-		console.log(`${title} has been liked: ${hasLiked}`);
-	}, [hasLiked]);
-
-	// const [varibleName, setVariableName] = useState()
-	// [initial value, updater function]
-	// const [hasLiked, setHasLiked] = useState(false);
-
-	return (
-		<div
-			className="card"
-			onClick={() => setCount((prevState) => prevState + 1)}
-		>
-			<h2>
-				{title} <br />
-				{count}
-			</h2>
-			<button onClick={() => setHasLiked((prevState) => !prevState)}>
-				{hasLiked ? '🤎' : '🤍'}
-			</button>
-		</div>
-	);
-}
+import { React, useState } from 'react';
+import Search from './components/Search';
 
 function App() {
+	const [searchTerm, setSearchTerm] = useState('');
+
 	return (
-		<div className="card-container">
-			<Card title="Star Wars" />
-			<Card title="Avatar" />
-			<Card title="Dune" />
-		</div>
+		<main>
+			<div className="pattern" />
+			<div className="wrapper">
+				<header>
+					<img src="./hero.png" alt="Hero Banner" />
+					<h1>
+						Find <span className="text-gradient">Movies</span> You'll Enjoy
+						Without the Hassle
+					</h1>
+				</header>
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			</div>
+		</main>
 	);
 }
 
